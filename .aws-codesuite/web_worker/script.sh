cff0d4a5257e6c58809c5f13973f1d14aa293709
Diego Felipe <dev6.seucondominio@gmail.com>
2025-09-09 08:59:37 -0300

    Create script.sh

---
+++ .aws-codesuite/web_worker/script.sh
@@ -0,0 +1,8 @@
+print "====> Seu Condominio (WEB+WORKER)"
+
+print "====> Files"
+ls -lta
+
+print "====> AWS artefatos (.json files)"
+# printf '{"ImageURI":"%s"}' 516862767124.dkr.ecr.us-east-1.amazonaws.com/seucondominio-erp:$NEW_TAG > imageDetail.json
+# printf '[{"name":"seucondominio-worker","imageUri":"%s"}]' 516862767124.dkr.ecr.us-east-1.amazonaws.com/seucondominio-erp:$NEW_TAG > imagedefinitions.json
